0a988817875ff9dd41b4eeb7325ebe154a7abd3d
Rodion Sibov <rodionsibov@gmail.com>
2021-05-11 22:42:25 +0300

    first commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h3>Add Todo</h3>
+    <div class="add">
+      <form @submit.prevent="onSubmit">
+        <input type="text" v-model="title" placeholder="Add Todo..." />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    ...mapActions(["addTodo"]),
+    onSubmit() {
+      this.addTodo(this.title);
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 10px;
+  border: 2px solid #41b883;
+  outline: 0;
+  border-radius: 5px 0 0 5px;
+}
+
+input[type="submit"] {
+  flex: 2;
+  background: #41b883;
+  color: #fff;
+  border: 1px solid #41b883;
+  cursor: pointer;
+  border-radius: 0 5px 5px 0;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span> <span class="incomplete-box"></span> = Incomplete </span>
+      <span> <span class="complete-box"></span> = Complete </span>
+    </div>
+    <div class="todos">
+      <div
+        @dblclick="onDblClick(todo)"
+        v-for="todo in allTodos"
+        :key="todo.id"
+        class="todo"
+        :class="{ 'is-complete': todo.completed }"
+      >
+        {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  },
+  methods: {
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updateTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed,
+      };
+
+      this.updateTodo(updateTodo);
+    },
+  },
+};
+</script>
+
+<style>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 1rem;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+</style>
